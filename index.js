46a87305e6840a293eeebf1a75c6310934358cad
Andressa Laurentino de Lima <karolsevillapasquarelli6@gmail.com>
2022-04-24 10:31:21 -0400

    alterações

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ principal();
 const path = require("path")
 const express = require("express");
 const app = express();
+const port = 2040;
 
+
+app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", express.static(path.join(__dirname, "public/")));
 
 
 
-app.listen(8080, () => {
-    
+app.listen(port, () => {
+  console.log("ta funcionando")  
 });
